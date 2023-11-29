4e4a498be2036e6527f7631ea6eaf6ac40b6b842
DG <daro.mush@gmail.com>
2023-11-29 14:09:17 +0100

    zadanie klasa produkt  commit

--- z1.cs
+++ z1.cs
@@ -10,6 +10,18 @@ using System.Data.SqlClient;
 
 namespace YourNamespace
 {
+    public class Product
+{
+    public string SKU { get; set; }
+    public string Name { get; set; }
+    public string EAN { get; set; }
+    public string Manufacturer { get; set; }
+    public string Category { get; set; }
+    public string ImageUrl { get; set; }
+    public int Stock { get; set; }
+    public string LogisticUnit { get; set; }
+    public decimal NetPrice { get; set; }
+}
     [ApiController]
     [Route("[controller]")]
     public class DGController : ControllerBase
